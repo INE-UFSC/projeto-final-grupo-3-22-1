93f1264047b8ffcac817dcf4f18fe02b39d97e42
arthur <aenartzr@gmail.com>
2022-06-26 13:42:07 -0300

    feat(enemy_test): agora também adiciona um jogador

--- prototipo/enemy_test.py
+++ prototipo/enemy_test.py
@@ -1,7 +1,9 @@
 import sys
 import pygame
 from pygame.locals import *
+from ControleInimigo import ControladorInimigo
 
+from Jogador import Jogador
 from Inimigo import Inimigo
 from Arma import Arma
 import numpy as np
@@ -9,7 +11,7 @@ import numpy as np
 #######################################
 # parte do pygame (ficara no app.py)
 pygame.init()
-FPS_VALUE = 60
+FPS_VALUE = 15
 FPS = pygame.time.Clock()
 
 SCREEN_WIDTH = 800
@@ -24,8 +26,11 @@ pygame.display.set_caption("Game")
 # propriedades iniciais do jogador
 VIDA = 20
 MOV_SPEED = 10
+arma_inicial = Arma(4, 0, 20, 5)
 
-inimigo = Inimigo(350, 350, 15)
+inimigo = Inimigo(350, 350, 15, 2, "tilapia.png")
+jogador = Jogador(VIDA, MOV_SPEED, arma_inicial, DISPLAY_SURF, SCREEN_WIDTH)
+controleInimigo = ControladorInimigo(lista_inimigos=[inimigo], jogador=jogador)
 
 while True:
     for event in pygame.event.get():
@@ -34,8 +39,12 @@ while True:
             sys.exit
 
     DISPLAY_SURF.fill((255, 255, 255))
-    DISPLAY_SURF.blit(inimigo.sprite, (inimigo.x, inimigo.y))
+    jogador.mover()
     inimigo.desenhar(DISPLAY_SURF)
+    controleInimigo.checar_colisao()
+    DISPLAY_SURF.blit(jogador.sprite, jogador.rect)
+    jogador.mover()
+
 
     pygame.display.update()
     FPS.tick(FPS_VALUE)
