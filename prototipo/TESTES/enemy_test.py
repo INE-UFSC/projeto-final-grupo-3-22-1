5a7b31dccb27ed0f15c04afc1a4e0468f52cdfb6
arthur <aenartzr@gmail.com>
2022-06-26 15:18:44 -0300

    feat(enemy_test): faz checagem e aciona uma interfaceMorte

---
+++ prototipo/TESTES/enemy_test.py
@@ -0,0 +1,76 @@
+import sys
+import pygame
+from pygame.locals import *
+import random as rd
+
+from ControleInimigo import ControladorInimigo
+from ControleJogador import ControleJogador
+from InterfaceMorte import InterfaceMorte
+
+from Jogador import Jogador
+from Inimigo import Inimigo
+from Arma import Arma
+import numpy as np
+
+#######################################
+# parte do pygame (ficara no app.py)
+pygame.init()
+FPS_VALUE = 15
+FPS = pygame.time.Clock()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+
+# tela
+DISPLAY_SURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+DISPLAY_SURF.fill((255, 255, 255))
+pygame.display.set_caption("Game")
+
+#######################################
+# propriedades iniciais do jogador
+VIDA = 20
+MOV_SPEED = 10
+arma_inicial = Arma(4, 0, 20, 5)
+
+inimigo = Inimigo(350, 350, 15, 2, "tilapia.png")
+jogador = Jogador(VIDA, MOV_SPEED, arma_inicial, DISPLAY_SURF, SCREEN_WIDTH)
+controleInimigo = ControladorInimigo(lista_inimigos=[inimigo], jogador=jogador)
+controleJogador = ControleJogador(jogador=jogador)
+interfaceMorte = InterfaceMorte(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+morto = False
+while True:    
+    if morto:
+        interfaceMorte.mostrar_tela(DISPLAY_SURF) # por algum motivo ele n sobrescreve a tela, 
+                                                  # e ai esse método só mostra a caption da janela
+        DISPLAY_SURF.blit(jogador.sprite, jogador.rect) # isso aqui era só pra deixar a imagem do Chico Cunha morto lá na tela de morte
+        continue # continue faz com que se esse if seja ativado, o while loop vai continuar aqui dentro e não passar pros próximos
+    
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit
+
+
+    DISPLAY_SURF.fill((255, 255, 255))
+    jogador.mover()
+    inimigo.desenhar(DISPLAY_SURF)
+    inimigo.mover(rd.choice(['x', 'y']), rd.choice([-1, 1]))
+    controleInimigo.checar_colisao() # checa colisão para diminuir da vida do jogador
+                                     # nesse ControleInimigo, eu usei tanto uma instância de Jogador qnto uma lista de inimigos
+                                     # provavelmente tem que mudar isso, acho q n seria legal o ControleInimigo ter um Jogador
+                                     # talvez uma classe associativa binária pra checar colisão e comunicações entre os 2?
+    DISPLAY_SURF.blit(jogador.sprite, jogador.rect)
+
+
+    if jogador.vida <= 0:
+        jogador.set_sprite("ChicoCunhaMorto.png")
+        DISPLAY_SURF.fill((255, 255, 255))
+        inimigo.desenhar(DISPLAY_SURF)
+        DISPLAY_SURF.blit(jogador.sprite, jogador.rect)
+        morto = True
+
+
+    pygame.display.update()
+    FPS.tick(FPS_VALUE)
