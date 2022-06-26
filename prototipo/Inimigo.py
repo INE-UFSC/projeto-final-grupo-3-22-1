299e2a29a0cb968634ab0dd964b61867cb5cfe09
arthur <aenartzr@gmail.com>
2022-06-26 13:42:47 -0300

    feat(Inimigo): alguns métodos aperfeiçoados; bugfixing

--- prototipo/Inimigo.py
+++ prototipo/Inimigo.py
@@ -1,3 +1,4 @@
+from re import X
 import pygame
 from pygame.locals import *
 import random as rd
@@ -5,7 +6,7 @@ import random as rd
 pygame.init()
 
 class Inimigo():
-    def __init__(self, x, y, velocidade):
+    def __init__(self, x, y, velocidade, dano, sprite):
         #self.vida = vida
         #self.velocidade = velocidade
         #self.tipo_ataque = tipo_ataque
@@ -16,29 +17,39 @@ class Inimigo():
         self.__x = x
         self.__y = y
         self.__velocidade = velocidade
-        self.__sprite = pygame.image.load("ChicoCunha.png")
+        self.__sprite = pygame.image.load(sprite)
+        self.__rect = self.__sprite.get_rect(center=(self.__x, self.__y))
+        self.__dano = dano
 
     def atacar(self):
         pass
 
     def mover(self, direcao, sentido):
         if direcao == 'x':
-            self.__x += sentido * self.__velocidade
+            self.__rect.x += sentido * self.__velocidade
         elif direcao == 'y':
-            self.__y += sentido * self.__velocidade    
+            self.__rect.y += sentido * self.__velocidade    
 
     def desenhar(self, win):
         self.mover(rd.choice(['x', 'y']), rd.choice([-1, 1]))
         win.blit(self.__sprite, (self.x, self.y))
     
     @property
-    def x(self):
-        return self.__x
+    def x(self) -> int:
+        return self.__rect.x
     
     @property
-    def y(self):
-        return self.__y
+    def y(self) -> int:
+        return self.__rect.y
     
     @property
     def sprite(self):
         return self.__sprite
+    
+    @property
+    def dano(self) -> int:
+        return self.__dano
+    
+    @property
+    def rect(self):
+        return self.__rect
