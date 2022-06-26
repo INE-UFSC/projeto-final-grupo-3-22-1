c7ae915d7abd7e75f7b0496f9e79a30a2d8f0b5e
arthur <aenartzr@gmail.com>
2022-06-26 15:19:39 -0300

    feat(repo): ControleJogador adicionado

---
+++ prototipo/TESTES/ControleJogador.py
@@ -0,0 +1,12 @@
+from Jogador import Jogador
+import pygame
+from pygame.locals import *
+
+class ControleJogador():
+    def __init__(self, jogador: Jogador):
+        self.__jogador = Jogador
+
+    def jogador_morto(self):
+        vida = self.__jogador.vida
+        if vida <= 0:
+            return True
