2f97ffa8811b636dbcabc74af4c5a3fb18692332
arthur <aenartzr@gmail.com>
2022-06-26 13:43:20 -0300

    feat(ControladorInimigo): adicionado método checar_colisao

---
+++ prototipo/ControladorInimigo.py
@@ -0,0 +1,9 @@
+from Inimigo import Inimigo
+from Jogador import Jogador
+
+class ControladorInimigo():
+    def __init__(self, inimigo: Inimigo, jogador_x: int, jogador_y):
+        self.inimigo = Inimigo
+        self.jogador_x = jogador_x
+        self.jogador_y = jogador_y
+        
