771f321c2df898ebeeec76ae81768309d05c6630
arthur <aenartzr@gmail.com>
2022-07-07 23:45:53 -0300

    feat(ControleInimigo): adicionei metodo para achar caminho em InimigoRastreador

--- versao_final/ControleInimigo.py
+++ versao_final/ControleInimigo.py
@@ -1,7 +1,9 @@
+from decimal import DivisionByZero
 from Inimigo import Inimigo
 from Jogador import Jogador
 import pygame
 from pygame.locals import *
+import numpy as np
 
 class ControladorInimigo():
     def checar_colisao(self, inimigo, jogador):
@@ -14,3 +16,25 @@ class ControladorInimigo():
                 
                 inimigo.vida -= bala.dano
                 print("dano")
+    
+    def achar_caminho(self, inimigo, jogador_x, jogador_y):
+        distx = jogador_x - inimigo.x
+        disty = jogador_y - inimigo.y
+
+        try:
+            angulo = np.arctan(disty/distx)
+        except DivisionByZero:
+            angulo = np.arctan(disty/1)
+        
+        xmov = inimigo.velocidade * np.sin(angulo)
+        ymov = inimigo.velocidade * np.cos(angulo)
+
+        if jogador_x >= inimigo.x and jogador_y >= inimigo.y:
+            inimigo.mover(xmov, ymov, sentidox = 1, sentidoy = 1)
+        elif jogador_x <= inimigo.x and jogador_y >= inimigo.y:
+            inimigo.mover(xmov, ymov, sentidox = -1, sentidoy = 1)
+        elif jogador_x >= inimigo.x and jogador_y <= inimigo.y:
+            inimigo.mover(xmov, ymov, sentidox = 1, sentidoy = -1)
+        else:
+            inimigo.mover(xmov, ymov, sentidox = -1, sentidoy = -1)
+
