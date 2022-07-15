72f1f47e11d1b3885dc7e8992a0fc2d1ad525e1a
scarpart <aenartzr@gmail.com>
2022-07-15 10:22:09 -0300

    feat(repo): melhorei o esqueleto do Vendedor.py
    fix(InimigoTeletransporador): tentando concertar suas mecânicas de movimento, mudei o nome também

--- versao_final/InimigoTeletransportador.py
+++ versao_final/InimigoTeletransportador.py
@@ -9,28 +9,34 @@ from Inimigo import Inimigo
 
 class InimigoTeletransportador(Inimigo):
     """
-    Classe que define inimigos atiradores, que evitam o jogador e atiram
-    projéteis de uma distância segura
+    Classe que define inimigos teletransportadores, surpreendem o Jogador ao
+    aparecer do nada para atacar e depois desaparecer
     """
     
     def __init__(self, x: int, y: int, velocidade: int,
-                    dano: int, sprite: str, vida=10):
+                    dano: int, sprite: str,
+                    cooldown_ataque: int, vida=10):
         super().__init__(x, y, velocidade,
                         dano, sprite, vida)
         
         self._pulo_completo = False
         self._sprite_original = self._sprite
-        self._cooldown_ataque = 300
+        self._cooldown_ataque = cooldown_ataque
         self._tempo_inicio_ataque = pygame.time.get_ticks()
+        self._atacando = False
 
         self._settings = Settings()
 
     def atacar(self, jogador_x, jogador_y):
         tempo_agora = pygame.time.get_ticks()
-        
-        if tempo_agora - self._tempo_inicio_ataque >= 300:
 
-    def mover(self, x, y):
+
+        if tempo_agora - self._tempo_inicio_ataque >= self._cooldown_ataque or self._atacando:
+            self._rect.x, self._rect.y = jogador_x, jogador_y
+
+
+
+    def mover(self, x: int, y: int):
         pass
 
     def desaparecer(self):
