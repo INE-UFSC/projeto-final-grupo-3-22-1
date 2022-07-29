69484f238221434ee7d83217dcd2703a167df37e
arthur <aenartzr@gmail.com>
2022-07-28 21:47:06 -0300

    fix(Boss): agora o ataque proximo funciona bem, mudei também os timers de ataques para ter um só no método atacar

--- versao_final/Boss.py
+++ versao_final/Boss.py
@@ -33,92 +33,77 @@ class Boss(pygame.sprite.Sprite):
         self._settings = Settings()
         self._globals = Globals()
 
-        self._tempo_ultimo_especial = 0
+        self._tempo_ultimo_atq = 0
         self._tempo_ultimo_mov = 0
-        self._tempo_ultimo_tiro = 0
 
     def atacar(self, jogador_x, jogador_y):
-        choice = rd.choice([0, 1])
-
-        if abs(jogador_x - self._rect.x) < 20:
-            return self._ataque_proximo(self._rect.x, self._rect.y)
-
-        if choice == 0:
-            return self._ataque_distancia(jogador_x, jogador_y)
-        return self._ataque_especial(jogador_x, jogador_y)
-
-    def _ataque_distancia(self, jogador_x, jogador_y):
-        """O InimigoAtirador vai atirar projéteis na direção do Jogador"""
-
-        # obtém o tempo quando o método é chamado, para comparar com o tempo salvo
-        # isso é a base do sistema de cadência de tiros
         tempo_agora = pygame.time.get_ticks()
 
-        # compara o tempo obtido com o tempo salvo para checar se está dentro do tempo de cadência
-        if tempo_agora - self._tempo_ultimo_tiro > 5000:
-            # atualiza o tempo
-            self._tempo_ultimo_tiro = tempo_agora
+        if tempo_agora - self._tempo_ultimo_atq >= 1000:
+            choice = rd.choice([0, 1])
 
-            # calcula as distâncias da posição do jogador à posição do inimigo
-            distancia_x = jogador_x - self._rect.x
-            distancia_y = jogador_y - self._rect.y
+            self._tempo_ultimo_atq = tempo_agora
 
-            # calcula o ângulo entre essas distâncias e com isso calcula a velocidade do projétil
-            angulo = atan2(distancia_y, distancia_x)
+            if abs(jogador_x - self._rect.x) < 200:
+                return self._ataque_proximo(self._rect.center[0], self._rect.center[1])
 
-            speed_x = self._vel_atq * cos(angulo)
-            speed_y = self._vel_atq * sin(angulo)
+            if choice == 0:
+                return self._ataque_distancia(jogador_x, jogador_y)
+            return self._ataque_especial(jogador_x, jogador_y)
 
-            # instancia uma nova Bala de acordo com as informações obtidas e retorna a mesma
-            incr = -0.05
-            balas_boss = []
-            for _ in range(5):
-                nova_bala = Bala(
-                    self._rect.x,
-                    self._rect.y,
-                    speed_x + incr,
-                    speed_y + incr,
-                    pygame.image.load("assets/isca.png"),
-                    3,
-                    20,
-                )
+    def _ataque_distancia(self, jogador_x, jogador_y):
+        # calcula as distâncias da posição do jogador à posição do inimigo
+        distancia_x = jogador_x - self._rect.x
+        distancia_y = jogador_y - self._rect.y
 
-                incr += 0.25
-                balas_boss.append(nova_bala)
+        # calcula o ângulo entre essas distâncias e com isso calcula a velocidade do projétil
+        angulo = atan2(distancia_y, distancia_x)
 
-            return balas_boss
+        speed_x = self._vel_atq * cos(angulo)
+        speed_y = self._vel_atq * sin(angulo)
+
+        # instancia uma nova Bala de acordo com as informações obtidas e retorna a mesma
+        incr = -0.05
+        balas_boss = []
+        for _ in range(5):
+            nova_bala = Bala(
+                self._rect.x,
+                self._rect.y,
+                speed_x + incr,
+                speed_y + incr,
+                pygame.image.load("assets/isca.png"),
+                3,
+                20,
+            )
+
+            incr += 0.25
+            balas_boss.append(nova_bala)
+
+        return balas_boss
 
     def _ataque_proximo(self, x, y):
 
-        tempo_agora = pygame.time.get_ticks()
-
-        signal = 1
-        while tempo_agora - self._tempo_comeco_ataque <= 500:
-            self._rect.x += signal
-            self._rect.y += signal
-            signal *= -1
+        #signal = 1
+        #self._rect.x += signal
+        #self._rect.y += signal
+        #signal *= -1
 
         return BombaVeneno(x, y, pygame.image.load("assets/mancha_veneno.png"), 5)
 
     def _ataque_especial(self, x, y):
 
-        tempo_agora = pygame.time.get_ticks()
-
-        if tempo_agora - self._tempo_ultimo_especial  > 1000:
-            self._tempo_ultimo_especial = tempo_agora
-
-            return BombaLava(
-                x + rd.choice([-50, -70, 50, 70]),
-                y + rd.choice([-50, -70, 50, 70]),
-                pygame.image.load("assets/mancha_lava.png"),
-                5,
-             )
+        return BombaLava(
+            x + rd.choice([-50, -70, 50, 70]),
+            y + rd.choice([-50, -70, 50, 70]),
+            pygame.image.load("assets/mancha_lava.png"),
+            5,
+            )
 
     def mover(self):
         
         tempo_agora = pygame.time.get_ticks()
 
-        if tempo_agora - self._tempo_ultimo_mov >= 500:
+        if tempo_agora - self._tempo_ultimo_mov >= 150:
             self._tempo_ultimo_mov = tempo_agora
 
             self._rect.x += rd.choice([-2, -1.5, -1, 1, 1.5, 2]) * self._vel_mov
